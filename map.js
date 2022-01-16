7c811b60f78304e3fb115d829ea442a8df116705
Jingr <jingruchen@outlook.com>
2022-01-16 17:32:25 -0500

    autocompletion

--- map.js
+++ map.js
@@ -1,20 +1,26 @@
 let map;
 
 function initMap() {
-  const directionsService = new google.maps.DirectionsService();
-  const directionsRenderer = new google.maps.DirectionsRenderer();
   const map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 42.274, lng: -71.808},
     zoom: 14,
   });  
+
+  const directionsService = new google.maps.DirectionsService();
+  const directionsRenderer = new google.maps.DirectionsRenderer();
   // *Render route
   directionsRenderer.setMap(map);
-
   const onChangeHandler = function () {
     calculateAndDisplayRoute(directionsService, directionsRenderer);
   };
-
   document.getElementById("submit").addEventListener("click", onChangeHandler);
+
+  const card = document.getElementById("pac-card");
+  const start = document.getElementById("start");
+  const end = document.getElementById("end");
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
+  new google.maps.places.Autocomplete(start);
+  new google.maps.places.Autocomplete(end);
 }
 
 function calculateAndDisplayRoute(directionsService, directionsRenderer) {
